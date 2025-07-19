8cd3ef7753ab14d3c91873b3ee4f21483f586e04
Otabek Makhmudov <mahmudovotabek2208@gmail.com>
2025-07-19 16:08:47 +0500

    first commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,85 @@
+import { it } from "node:test";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaChartLine, FaHistory, FaRegUser } from "react-icons/fa";
+import { FiMessageCircle } from "react-icons/fi";
+import { IoSettingsOutline, IoStatsChart } from "react-icons/io5";
+import { PiHandbagLight } from "react-icons/pi";
+import { RiHomeFill } from "react-icons/ri";
+import { VscSignOut } from "react-icons/vsc";
+
+export default function Sidebar() {
+  const menuData = [
+    {
+      name: "Dashboard",
+      isDisabled: false,
+      icon: <RiHomeFill />,
+    },
+    {
+      name: "Profile",
+      isDisabled: true,
+      icon: <FaRegUser />,
+    },
+    {
+      name: "Leaderboard",
+      isDisabled: true,
+      icon: <IoStatsChart />,
+    },
+    {
+      name: "Order",
+      isDisabled: true,
+      icon: <AiOutlineShoppingCart />,
+    },
+    {
+      name: "Product",
+      isDisabled: true,
+      icon: <PiHandbagLight />,
+    },
+    {
+      name: "Sales Report",
+      isDisabled: true,
+      icon: <FaChartLine />,
+    },
+    {
+      name: "Message",
+      isDisabled: true,
+      icon: <FiMessageCircle />,
+    },
+    {
+      name: "Settings",
+      isDisabled: true,
+      icon: <IoSettingsOutline />,
+    },
+    {
+      name: "Favorite",
+      isDisabled: true,
+      icon: <IoSettingsOutline />,
+    },
+    {
+      name: "History",
+      isDisabled: true,
+      icon: <FaHistory />,
+    },
+    {
+      name: "Signout",
+      isDisabled: true,
+      icon: <VscSignOut />,
+    },
+  ];
+
+  return (
+    <aside className="w-[200px] h-screen p-4 border-solid border-r border-[#2C2D33] ">
+      {menuData.map((item) => {
+        return (
+          <button
+            key={item.name}
+            disabled={item.isDisabled}
+            className="flex items-center gap-4 text-[#87888C] hover:text-[#000] hover:bg-[#A9DFD8] disabled:cursor-not-allowed hover:disabled:bg-[#a9dfd8bb] hover:font-bold duration-300 cursor-pointer mb-4 px-4 py-2 rounded-md w-full"
+          >
+            <span>{item.icon}</span>
+            <span>{item.name}</span>
+          </button>
+        );
+      })}
+    </aside>
+  );
+}
